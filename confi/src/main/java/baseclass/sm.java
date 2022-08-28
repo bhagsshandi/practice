2f85c581c6563b7cbb67a169a8ed5f581eeaa290
admin <admin@LAPTOP-C3ULJPGS>
2022-08-28 16:09:58 +0530

    first commit

---
+++ confi/src/main/java/baseclass/sm.java
@@ -0,0 +1,23 @@
+package baseclass;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+public class sm {
+	
+	@Test
+	public void testse() throws Exception {
+		File fi=new File("C:\\Users\\admin\\eclipse-workspace\\confi\\src\\test\\resources\\proper\\configuration.properties");
+		FileInputStream fie=new FileInputStream(fi);
+		Properties pro=new Properties();
+		pro.load(fie);
+		String path=pro.getProperty("browser");
+		System.out.println(path+"=======");
+		
+	}
+
+}
